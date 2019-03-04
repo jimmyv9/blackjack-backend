e603120c06a935a52890a98fa356681e675e7aa8
jimmyv9 <joaopvapv100@yahoo.com.br>
2019-03-03 22:48:19 -0600

    CMake compiled version
    Executable named "blackjack" added
    CMakeFiles from added from project in CLion IDE

---
+++ cmake-build-debug/CMakeFiles/blackjack.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/JPV/Documents/Joao/Rhodes/2017-2018/2017 Fall/Computer Science/1_Veloso_Joao/card.cpp" "/Users/JPV/Documents/Joao/Rhodes/2017-2018/2017 Fall/Computer Science/1_Veloso_Joao/cmake-build-debug/CMakeFiles/blackjack.dir/card.cpp.o"
+  "/Users/JPV/Documents/Joao/Rhodes/2017-2018/2017 Fall/Computer Science/1_Veloso_Joao/deck.cpp" "/Users/JPV/Documents/Joao/Rhodes/2017-2018/2017 Fall/Computer Science/1_Veloso_Joao/cmake-build-debug/CMakeFiles/blackjack.dir/deck.cpp.o"
+  "/Users/JPV/Documents/Joao/Rhodes/2017-2018/2017 Fall/Computer Science/1_Veloso_Joao/hand.cpp" "/Users/JPV/Documents/Joao/Rhodes/2017-2018/2017 Fall/Computer Science/1_Veloso_Joao/cmake-build-debug/CMakeFiles/blackjack.dir/hand.cpp.o"
+  "/Users/JPV/Documents/Joao/Rhodes/2017-2018/2017 Fall/Computer Science/1_Veloso_Joao/human.cpp" "/Users/JPV/Documents/Joao/Rhodes/2017-2018/2017 Fall/Computer Science/1_Veloso_Joao/cmake-build-debug/CMakeFiles/blackjack.dir/human.cpp.o"
+  "/Users/JPV/Documents/Joao/Rhodes/2017-2018/2017 Fall/Computer Science/1_Veloso_Joao/main.cpp" "/Users/JPV/Documents/Joao/Rhodes/2017-2018/2017 Fall/Computer Science/1_Veloso_Joao/cmake-build-debug/CMakeFiles/blackjack.dir/main.cpp.o"
+  "/Users/JPV/Documents/Joao/Rhodes/2017-2018/2017 Fall/Computer Science/1_Veloso_Joao/player.cpp" "/Users/JPV/Documents/Joao/Rhodes/2017-2018/2017 Fall/Computer Science/1_Veloso_Joao/cmake-build-debug/CMakeFiles/blackjack.dir/player.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/blackjack.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blackjack.dir/main.cpp.o"
+  "CMakeFiles/blackjack.dir/card.cpp.o"
+  "CMakeFiles/blackjack.dir/deck.cpp.o"
+  "CMakeFiles/blackjack.dir/hand.cpp.o"
+  "CMakeFiles/blackjack.dir/human.cpp.o"
+  "CMakeFiles/blackjack.dir/player.cpp.o"
+  "blackjack.pdb"
+  "blackjack"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blackjack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
